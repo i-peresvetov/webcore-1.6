147095847a788ccea9a558e5d030d9d574caa8a6
Ivan Peresvetov <lumenist@mail.ru>
2023-10-08 21:38:35 +0300

    first scss and vars

--- README.md
+++ README.md
@@ -1,126 +1,28 @@
-# Webpack Static Site Starter Kit
+# WEBPACK
 
-## Требования к проекту
+ссылка на youtube
+https://youtu.be/o8KMucDpSno
 
-- [Node 14.18.3](https://nodejs.org/download/release/v14.18.3/)
-- [NVM/MacOC](https://tecadmin.net/install-nvm-macos-with-homebrew/)
-- [NVM/Windows](https://github.com/coreybutler/nvm-windows/releases) Если Windows то установка только от имени администратора
-- [Yarn](https://yarnpkg.com/)
+Для установки пакетов используйте команду npm install
 
----
+## Команды
 
-## Использование
-
-Клонировать репозиторий
-
-```bash
-git clone https://github.com/alex-karo/webpack-static-template block7
-```
-
-В папке block7 перейти в папку проекта
-
-```bash
-cd block7
-```
-
-Удалить папку .git
-
-```bash
-rm -rf .git
-```
-
----
-
-## Если создаете новый проект в github.com
-
-нужно будет инициализировать git проект, _если данная команда не сработает значит версия git установлена старая, нужно обновить её_
-
-```properties
-git init -b main
-```
-
-добавить все изменения и сделать первый коммит
-
-```properties
-git add . && git commit -m "initial commit"
-```
-
-соединить git удаленный с локальным, *USER* (пользователь) и *REPO* (репозиторий)
-
-```properties
-git remote add origin https://github.com/USER/REPO.git
-```
-
-проверить на то что есть соединение
-
-```properties
-git remote -v
-```
-
-отправить изменения в удаленный репозиторий
-
-```properties
-git push origin main
-```
-
----
-
-## Проверка версии ноды
-
-Должно вывести версию ноды 14.18.3
-
-```properties
-node -v
-```
-
----
-
-## Два метода установки зависимостей
-
-Установка зависимостей через NPM
-
-```properties
-npm install
-```
-
-Установка зависимостей через Yarn
-
-```properties
-yarn
-```
-
----
-
-## Два метода запуска приложения
-
-Для запуска веб-сервера через NPM
-
-```properties
+### Запуск сервера для разработки
+```shell
 npm run start
 ```
 
-Для запуска веб-сервера через Yarn
-
-```properties
-yarn start
+### Сборка проекта без оптимизации
+```shell
+npm run build-dev
 ```
 
----
-
-## Два метода загрузки на Github Pages
-
-### Ваш репозиторий должен быть связан с github.com репозиторием, после этого выполняем
-
-Загрузка на Github Pages через NPM
-
-```properties
-npm run deploy
+### Сборка проекта с оптимизацией
+```shell
+npm run build-prod
 ```
 
-Загрузка на Github Pages через Yarn
-
-```properties
-yarn deploy
+### Очистка папки dist
+```shell
+npm run clear
 ```
-
-Основано на [этой статье](https://hackernoon.com/lets-start-with-webpack-4-91a0f1dba02e)
--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,117 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
-  // Входной файл
-  entry: [
-    './src/js/index.js'
-  ],
-
-  // Выходной файл
-  output: {
-    filename: './js/bundle.js'
+  mode,
+  target,
+  devtool,
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
   },
-
-  // Source maps для удобства отладки
-  devtool: "source-map",
-
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    // new CopyPlugin({
+    //   patterns: [{ from: 'static', to: './' }],
+    // }),
+  ],
   module: {
     rules: [
-      // Транспилируем js с babel
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
-        exclude: /node_modules/,
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              },
+            },
+          },
+          'group-css-media-queries-loader',
+          {
+            loader: 'resolve-url-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: devMode
+          ? []
+          : [
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  mozjpeg: {
+                    progressive: true,
+                  },
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: [0.65, 0.9],
+                    speed: 4,
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                  webp: {
+                    quality: 75,
+                  },
+                },
+              },
+            ],
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-          }
-        }
-      },
-
-      // Компилируем SCSS в CSS
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader, // Extract css to separate file
-          'css-loader', // translates CSS into CommonJS
-          'postcss-loader', // parse CSS and add vendor prefixes to CSS rules
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
-        ],
-      },
-
-      // Подключаем шрифты из css
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./fonts/[name].[ext]'
           },
-        ]
-      },
-
-      // Подключаем картинки из css
-      {
-        test: /\.(svg|png|jpg|jpeg|webp)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./static/[name].[ext]'
-          },
-        ]
+        },
       },
     ],
   },
-  plugins: [
-    // Подключаем файл html, стили и скрипты встроятся автоматически
-    new HtmlWebpackPlugin({
-      title: 'Webpack 4 Starter',
-      template: './src/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false,
-      }
-    }),
-
-    // Кладем стили в отдельный файлик
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-
-    // Копируем картинки
-    new CopyWebpackPlugin([
-      {
-        from: './src/img',
-        to: 'img',
-      },
-    ])
-  ],
 };
