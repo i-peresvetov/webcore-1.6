c157800d5ad50df4163918c5abf7131b38c1038f
Ivan Peresvetov <lumenist@mail.ru>
2023-10-17 16:29:04 +0300

    prices-table

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './index.html';
 import './index.scss';
 
-import './modules/expand-btn';
 import './modules/js_swiper.min';
 import './modules/swiper';
 import './modules/left-menu';
+import './modules/expand-btn';
 import './modules/modal';
